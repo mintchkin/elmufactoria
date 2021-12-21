c4269fb7b8fb595170ee7f51866daed20ad4713f
mintchkin <mintchkin.dobfner@gmail.com>
2021-12-21 18:57:12 -0500

    Fix transparency issue with tooltip styling

--- src/Layout.elm
+++ src/Layout.elm
@@ -12,36 +12,36 @@ import Html.Attributes as HA
 
 leftToolTip : String -> Attribute msg
 leftToolTip tip =
+    let
+        disablePointerEvents =
+            htmlAttribute <| HA.style "pointer-events" "none"
+    in
     onLeft <|
-        row
+        el
             [ centerY
-            , spacing -2
+            , disablePointerEvents
             , mouseOver [ transparent True ]
-            , htmlAttribute <| HA.style "pointer-events" "none"
+            , moveLeft 20
+            , padding 5
+            , Background.color (rgb 1 1 1)
+            , Border.color (rgb 0 0 0)
+            , Border.widthEach { top = 2, right = 0, bottom = 2, left = 2 }
+            , behindContent <|
+                el
+                    [ alignRight
+                    , centerY
+                    , disablePointerEvents
+                    , height (px 24)
+                    , moveRight 12
+                    , rotate (degrees 45)
+                    , width (px 24)
+                    , Background.color (rgb 1 1 1)
+                    , Border.color (rgb 0 0 0)
+                    , Border.widthEach { top = 2, right = 2, bottom = 0, left = 0 }
+                    ]
+                    none
             ]
-            [ el
-                [ padding 5
-                , Background.color (rgb 1 1 1)
-                , Border.color (rgb 0 0 0)
-                , Border.widthEach { top = 2, right = 0, bottom = 2, left = 2 }
-                ]
-                (text tip)
-            , el
-                [ centerY
-                , moveLeft 10
-                , width (px 24)
-                , height (px 24)
-                , Border.color (rgb 0 0 0)
-                , Border.widthEach
-                    { top = 2
-                    , right = 2
-                    , bottom = 0
-                    , left = 0
-                    }
-                , rotate (degrees 45)
-                ]
-                none
-            ]
+            (text tip)
 
 
 attachTooltip : String -> Attribute msg
